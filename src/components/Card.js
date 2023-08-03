b2a67a9016e897620faddc0beb13cab907036bf1
rohit <rohitchouhan3512@gmail.com>
2023-08-04 01:14:04 +0530

    modified grid view

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,31 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Card.css';
 
 const Card = ({ event }) => {
+  const [imageError, setImageError] = useState(false);
+
+  const fallbackImage =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwsrmtZlb0Bn_ARxNWoLw0Ke9eTybjTcKS_Q&usqp=CAU';
+
+  const image_url = imageError ? fallbackImage : event.banner_image || fallbackImage;
+
+  const handleImageError = () => {
+    setImageError(true);
+  };
+
   return (
     <div className="event-card">
       <div className="event-image">
-        <img src={event.banner_image} alt={event.event_name} />
+        <img src={image_url} alt={event.event_name} onError={handleImageError} />
       </div>
       <div className="event-details">
-        <h3>{event.event_name}</h3>
+        {/* 
         <div className="event-info">
-          <p>
-            <strong>City:</strong> {event.location}
-          </p>
-          <p>
-            <strong>Start Date:</strong> {event.start_time}
-          </p>
           <p>
             <strong>End Date:</strong> {event.end_time}
           </p>
-          <p>
-            <strong>Description:</strong> {event.description}
-          </p>
-          <p>
-            <strong>Category:</strong> {event.category}
-          </p>
+        </div> */}
+
+        <div className='info-section'>
+          <div className='date-info'>
+            <strong><p>{event.start_time}</p></strong>
+            <p>{event.category}</p>
+          </div>
+          <div className='name-info'>
+            <p>
+              <strong>{event.event_name}</strong>
+              <p>{event.location}</p>
+            </p>
+          </div>
         </div>
+
+        <div className='description-info'>
+        <p>{event.description}</p>
+        </div>
+        
       </div>
     </div>
   );
