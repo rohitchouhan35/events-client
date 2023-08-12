b8be0319aeef492c34fbdff187ad091673abd344
rohit <rohitchouhan3512@gmail.com>
2023-08-12 19:57:46 +0530

    modified structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import './Header.css';
 import { NavLink, useNavigate } from 'react-router-dom';
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Header = () => {
+  const { logout, loginWithRedirect, user, isAuthenticated, isLoading } = useAuth0();
   const navigate = useNavigate();
-  const [isSignedIn, setIsSignedIn] = useState(false);
-  const urlParams = new URLSearchParams(window.location.search);
-  const code = urlParams.get('code');
-  
-  useEffect(() => {
-    if (code) {
-      localStorage.setItem('userToken', code);
-      setIsSignedIn(true);
-    } else {
-      setIsSignedIn(false);
-    }
-  }, [code]);
 
   const handleCreate = () => {
-    if (!isSignedIn) {
+    if (!isAuthenticated) {
       alert("Please Sign in to create a new event");
       return;
     } else {
       navigate("/addevent");
     }
-  };
-
-  const handleSignOut = () => {
-    localStorage.removeItem('userToken');
-    setIsSignedIn(false);
-    navigate("/");
   };
 
   return (
@@ -38,16 +22,29 @@ const Header = () => {
         <h1>Allevent</h1>
       </div>
       <div className='header-btn'>
+      <div>
+  {isAuthenticated ? (
+    <div className='profile'>
+      <div className='profile-picture'>
+        <img className='profile-image' src={user.picture} alt={user.name} />
+      </div>
+      <div className='profile-details'>
+        <h2 className='profile-name'>{user.name}</h2>
+        <button onClick={handleCreate} className="create-event-button">Create Event</button>
+      </div>
+    </div>
+  ) : (
+    <p>Please Sign in to create event.</p>
+  )}
+</div>
+
         <div>
-          <button onClick={handleCreate} className="create-event-button">Create Event</button>
-        </div>
-        <div>
-          {isSignedIn ? (
-            <button onClick={handleSignOut} className="signin-button">Sign out</button>
+          {isAuthenticated ? (
+            <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+              Sign Out
+            </button>
           ) : (
-            <NavLink to="https://accounts.google.com/o/oauth2/v2/auth?response_type=code&access_type=online&client_id=224762843069-o6jpcp7unjcf4h519fjr8v6qk6b2jo1h.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&state&scope=profile%20email&approval_prompt=auto" key="addevent">
-              <button className="signin-button">Sign in</button>
-            </NavLink>
+              <button onClick={() => loginWithRedirect()} className="signin-button">Sign In</button>
           )}
         </div>
       </div>
--- src/services/eventData.js
+++ src/services/eventData.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
-const GET_URL = 'https://loggerheaded-storie.000webhostapp.com/all-events.php';
-const POST_URL = 'https://loggerheaded-storie.000webhostapp.com/add-new-events.php';
+// const GET_URL = 'https://loggerheaded-storie.000webhostapp.com/all-events.php';
+// const POST_URL = 'https://loggerheaded-storie.000webhostapp.com/add-new-events.php';
+const GET_URL = 'http://localhost/allevents/list-all-events.php';
+const POST_URL = 'http://localhost/allevents/add-new-events.php';
 
 class eventService {
+  
   static saveEvent(event) {
     return axios.post(POST_URL, event);
   }
