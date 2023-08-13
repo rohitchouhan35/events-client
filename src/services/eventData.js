2395498f7eb08710a9a8c93814ce7495d3223452
rohit <rohitchouhan3512@gmail.com>
2023-08-13 05:43:44 +0530

    added methods to store and fetch users

--- src/services/eventData.js
+++ src/services/eventData.js
@@ -1,21 +1,37 @@
 import axios from 'axios';
 
 const BASE_URL_HOSTED = "https://loggerheaded-storie.000webhostapp.com";
-const GET_URL = BASE_URL_HOSTED + '/list-all-events.php';
-const POST_URL = BASE_URL_HOSTED + '/add-new-events.php';
+
+const GET_EVENT_LIST_URL = BASE_URL_HOSTED + '/list-all-events.php';
+
+const ADD_EVENT_URL = BASE_URL_HOSTED + '/add-new-events.php';
+const ADD_USER_URL = BASE_URL_HOSTED + '/add-new-user.php';
+const GET_USER_LIST_URL = BASE_URL_HOSTED + '/list-all-users.php';
+
 
 // const BASE_URL_LOCAL = "http://localhost/events-server-main";
-// const GET_URL = BASE_URL_LOCAL + '/list-all-events.php';
-// const POST_URL = BASE_URL_LOCAL + '/add-new-events.php';
+
+// const GET_EVENT_LIST_URL = BASE_URL_LOCAL + '/list-all-events.php';
+// const ADD_EVENT_URL = BASE_URL_LOCAL + '/add-new-events.php';
+// const ADD_USER_URL = BASE_URL_LOCAL + '/add-new-user.php';
+// const GET_USER_LIST_URL = BASE_URL_LOCAL + '/list-all-users.php';
 
 class eventService {
   
   static saveEvent(event) {
-    return axios.post(POST_URL, event);
+    return axios.post(ADD_EVENT_URL, event);
   }
 
   static getEventList() {
-    return axios.get(GET_URL);
+    return axios.get(GET_EVENT_LIST_URL);
+  }
+
+  static saveUser(user) {
+    return axios.post(ADD_USER_URL, user);
+  }
+
+  static getUserList() {
+    return axios.get(GET_USER_LIST_URL);
   }
 }
 
